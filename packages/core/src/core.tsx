57e5b9a8864a1c734097d59c7d4f9d4aa15dfe09
wgw <wanggw01@mingyuanyun.com>
2022-05-26 22:42:58 +0800

    feat: test2

--- packages/core/src/core.tsx
+++ packages/core/src/core.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface Props {}
 
 function Core(props: Props) {
-  return <div>Component content2</div>;
+  return <div>Component content32</div>;
 }
 
 export default Core;
